55ca33174e83cbdaf6ce6fb78286f1a3f0e97255
komalkadam12 <55316550+komalkadam12@users.noreply.github.com>
2019-09-16 10:54:27 +0530

    initial commit

---
+++ sample.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Registration Form</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+
+    <h2>Registration Form</h2>
+
+    Rajnikant Code 
+	
+	Komal Code
+
+</body>
+</html>
